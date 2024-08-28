3f2f9888e58d3d28e0312cfa831db5ac729910d7
milenaljhhh <105796589+milenaljhhh@users.noreply.github.com>
2024-08-28 08:45:49 -0300

    nb

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ const textoResultado = document.querySelector('.texto-resultado');
 
 const alternativas = [
     {
-        enunciado: "No âmbito social, você prefere:",
+        enunciado: "No âmbito social, você prefere: salvar muitas pessoas de um ataque alienigina ou matar muitas pessoas com alinigenas sem nenhuma recompensa ",
         alternativa: [
             {
-                texto: "Altenativa 1",
-                afirmacao: "Afirmação 1",
+                texto: "está ocorrendo um incendio na sua escola, e alguem não consegue sair do local, mas você decide ajudar",
+                afirmacao: "você ajuda ela a escapar do incêndio, e vocês saem sem se machucar",
             },
             {
-                texto: "Altenativa 2",
-                afirmacao: "Afirmação 2",
+                texto: "sua mãe esta discutindo com você sobre seu futuro na faculdade, mas ela não de dar opção de escolha, por causa da questão financeira, no qual ela que vai pagar",
+                afirmacao:"voce prefere não fazer nenhum curso sem nenhuma garantia de empregabilidade, sendo expulso de casa"
             },
         ]
     },
@@ -23,21 +23,21 @@ const alternativas = [
         enunciado: "No âmbito ambiental, você prefere:",
         alternativa: [
             {
-                texto: "Altenativa 3",
-                afirmacao: "Afirmação 3",
+                texto: "seu õnibus quebrou e vcoe ta atrasada para a escola, mas um estranho de oferece carona, para ir no jadim botanico e conhecer várias briofitas",
+                afirmacao: "voce prefere aceita, com probababilidade da pessoa te sequestrar ou ir andando para escola do cabral até o estadual do paraná, para estudar sobre briófitas",
             },
             {
-                texto: "Altenativa 4",
-                afirmacao: "Afirmação 4",
+                texto: "Voce tirou nota baixa no boletim na materia que voce estava estudando sobre gimnospermas um tipo planta, e esá com medo de contar para sua mãe, porque você tem certesa que ela vai surtar",
+                afirmacao: "você prefere contar para ela ou não contar, mas quando ela descubrir, vai ser pior porque não tinha contado antes",
             },
         ]
     },
     {
         enunciado: "No âmbito tecnológico, você prefere:",
-        alternativa: [
+        alternativa: [ "ajudar milhares de pessoas com uma tecnologia que pssibiltaria ninguyem mais passa fome, ou lucrar com essa tecnolgia que so beneficiaria voce, e quem você quiser"
             {
-                texto: "Altenativa 5",
-                afirmacao: "Afirmação 5",
+                texto: "alunos de escolas não estão conseguindo acessar um site da escola para realizar uma atividade, mas voce tem a solução tecnológica para o problema,",
+                afirmacao: "voce prefere ajudar elas, ou não, e com iss só voce se beneficiaria nessa situação"
             },
             {
                 texto: "Altenativa 6",
